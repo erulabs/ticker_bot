c20226ea5e4f68f5a318e3c20b2b4a0b65347b25
Your NameKelly <krschlater@gmail.com>
2017-12-22 23:41:53 -0700

    Made this ticker bot. gets the junkies their juice in Discord

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log(`another ${2+2-3} word`); 
+//Ticker bots
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content.indexOf('tick') === 0) {
+    request( 'https://api.coinmarketcap.com/v1/ticker/', function(error, response, body){
+      let coinmarketcapdata = JSON.parse(body)
+      let marketdata = coinmarketcapdata.find(function(item){
+        let term = message.content.split(' ')[1]
+        if (term){
+          return(item.symbol.toLowerCase() == term.toLowerCase())
+        }
+      })
+      if (marketdata){
+        message.reply(marketdata.price_usd)
+      } else {
+        message.reply('No such coin')
+      }
+    })
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+//console.log(process.env.DISCORD_TOKEN);
+
+const request = require('request');
